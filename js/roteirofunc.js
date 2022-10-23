969a746c936e3d4f3c6bbfb694cb30e1f9db33b3
lucas-s-a <lucassalves485@outlook.com>
2022-10-23 13:34:28 -0300

    first commit

---
+++ js/roteirofunc.js
@@ -0,0 +1,24 @@
+// lista de espera get's
+let lenome = document.querySelector("#listespnome").value
+let leemail = document.querySelector("#listespemail").value
+let lesenha = document.querySelector("#listespsenha").value
+let lesenha2 = document.querySelector("#listespsenha2").value
+let lecep = document.querySelector("#listescep").value
+let leest = document.querySelector("#estado").value
+let lecid = document.querySelector("#cidade").value
+let lebairro = document.querySelector("#bairro").value
+let lerua = document.querySelector("#rua").value
+let lenumero = document.querySelector("#numero").value
+let lecomplet = document.querySelector("#complemento").value
+
+// login get's
+
+let lemail = document.querySelector("#loginemail").value
+let lsenha = document.querySelector("#loginsenha").value
+let lresposta = document.querySelector("#logadocomsucesso").value
+
+
+// Recuperar senha get's
+
+let rsrec = document.querySelector("#eRsenha").value
+let rsresp = document.querySelector("#MensagemResposta").value
