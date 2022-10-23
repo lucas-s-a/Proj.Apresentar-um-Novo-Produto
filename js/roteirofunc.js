0e785621a1c0a37bd563d3c366c025f877dbd283
lucas-s-a <lucassalves485@outlook.com>
2022-10-23 13:39:21 -0300

    Edicoes commit

--- js/roteirofunc.js
+++ js/roteirofunc.js
@@ -1,5 +1,5 @@
 // lista de espera get's
-let lenome = document.querySelector("#listespnome").value
+/*let lenome = document.querySelector("#listespnome").value
 let leemail = document.querySelector("#listespemail").value
 let lesenha = document.querySelector("#listespsenha").value
 let lesenha2 = document.querySelector("#listespsenha2").value
@@ -11,14 +11,22 @@ let lerua = document.querySelector("#rua").value
 let lenumero = document.querySelector("#numero").value
 let lecomplet = document.querySelector("#complemento").value
 
-// login get's
-
-let lemail = document.querySelector("#loginemail").value
-let lsenha = document.querySelector("#loginsenha").value
-let lresposta = document.querySelector("#logadocomsucesso").value
 
 
 // Recuperar senha get's
 
-let rsrec = document.querySelector("#eRsenha").value
-let rsresp = document.querySelector("#MensagemResposta").value
+let rsrec = document.querySelector("#eRsenha")
+let rsresp = document.querySelector("#MensagemResposta")*/
+
+// login get's
+const logini = document.querySelector("#logou")
+function lg(){
+    let s = document.querySelector("#corpo");
+    s.innerHTML = `
+    <main id="corpo2">
+        <div>
+            <img src="imagens2/Su.png" alt="logado">
+        </div>
+    </main>`;
+}
+logini.addEventListener('click',lg)
