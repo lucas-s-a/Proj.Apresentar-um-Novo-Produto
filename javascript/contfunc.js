c5f3400f68a3451326a65418dc3c30afa7566235
lucas-s-a <lucassalves485@outlook.com>
2022-10-27 22:31:48 -0300

    commit grupo edicao 4

--- javascript/contfunc.js
+++ javascript/contfunc.js
@@ -23,7 +23,7 @@ contbtn.addEventListener('click', async(e) =>{
                             <span aria-hidden="true">&times;</span>
                           </button>
                     </div>
-                    <div class="modal-body">Obrigado pelo comentário nós valorizamos cada feedback recebido</div>
+                    <div class="modal-body">Obrigado pelo comentário.<br> Nós valorizamos cada feedback recebido</div>
                     <div class="modal-footer">
                         <button type="button" class="btn text-white" style="background-color: #118CD9;" data-dismiss="modal">Fechar</button>
                     </div>
@@ -33,9 +33,20 @@ contbtn.addEventListener('click', async(e) =>{
         `
         
     }else{
+        e.preventDefault()
         console.log("deu ruim!")
         let mr = document.querySelector("#Recadastrei");
-        mr.innerHTML = ``
+        mr.innerHTML = `<br>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+             Algum campo está vazio <br>
+             Verifique se: <br>
+             O E-mail digitado é válido. <br>
+             Se os campos de telefone e celular estão preenchidos. <br>
+             Se o campo nome está preenchido<br>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+        </div>`
     }
     
     
